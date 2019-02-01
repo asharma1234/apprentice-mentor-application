e850251cba3bd2c023fc30241794ab04e13c86df
AnujaSharma <anuja@jyaasa.com>
2019-02-01 16:22:47 +0545

    Code refactored

--- apprentice.rb
+++ apprentice.rb
@@ -1,12 +1,12 @@
 class Apprentice
     attr_accessor :first_name, :last_name, :dob, :phone, :email
     
- def initialize(first_name = "", last_name = "", dob = "", phone = "", email = "")
-     @first_name = first_name
-     @last_name = last_name
-     @dob = dob
-     @phone = phone
-     @email = email
+def initialize(first_name = "", last_name = "", dob = "", phone = "", email = "")
+    @first_name = first_name
+    @last_name = last_name
+    @dob = dob
+    @phone = phone
+    @email = email
  
   end #end of initialize
  end #end of class Details
